ddd8392169a33b73318ce2be8dde7d903ea06f97
Divine Udise <divineoffishal@gmail.com>
2023-10-19 18:59:18 +0100

    Initial  commit

---
+++ components/card.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="h-auto w-64 border border-green-700">
+    <SanityImage
+    :asset-id="image"
+    auto="format"
+    class="h-72 w-64"
+  />    
+  <span class="pl-3">
+      <h1 class="font-bold text-lg text-green-700">{{ title }}</h1>
+      <h1>{{ price }}</h1>
+    </span>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: String,
+  image: String,
+  price: Number,
+  desc: String
+})
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="h-screen w-screen">
+    <h1 class="text-6xl font-bold text-center my-10 text-green-700">Welcome to a random fashion store</h1>
+    <div class="grid grid-cols-4 justify-items-center gap-y-5">
+      <Card v-for="(product, i) in data" :key="i" :title="product.name" :price="product.price" :desc="product.desc" :image="product.image.asset._ref"/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+  const query = `*[_type == 'product']{name, price, desc, image}`
+  const { data } = useSanityQuery(query)
+
+
+</script>
